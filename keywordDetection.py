bee06b33b1e54cec52484e7322ad8555b7972185
SebastianLlB <sebasllb01@gmail.com>
2025-03-07 18:25:41 -0600

    keywordDetection.py

--- keywordDetection.py
+++ keywordDetection.py
@@ -4,8 +4,8 @@ import struct
 
 '''Access Porcupine Api'''
 porcupine = pvporcupine.create(
-  access_key='nQznlljF2uUHKiHaVrKT2kdWNaTuSJEq6gMCKcMsIaBzhHLQ6B6t9A==',
-  keyword_paths=['C:\\Users\\USER\\Desktop\\KeyWords\\Friday_en_windows_v3_0_0\\Friday.ppn']
+  access_key='secret-api-key',
+  keyword_paths=['Path-to-keyword-file']
 )
 
 pa = pyaudio.PyAudio()
@@ -23,15 +23,15 @@ print("Listening for wake word...")
 try:
     '''As long as the Input is hearing, check for the Key Word and label the index as such'''
     while True:
-        # Read a frame from the microphone
+        '''Take a single frame from the microphone'''
         pcm = audio_stream.read(porcupine.frame_length)
         '''Unpack said frame'''
         pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
 
-        # Process the frame with Porcupine
+        '''Let porcupine process the frame'''
         keyword_index = porcupine.process(pcm)
 
-        # Check if the frame contains the Key Word
+      '''If porcupine detects the frame to be the keyword, then let the user know'''
         if keyword_index >= 0:
             print("recognized keywords")
             '''Once the Key Word is detected, then close the input stream'''
@@ -39,7 +39,6 @@ try:
 
 finally:
     '''Once the Key Word is detected, then close the input stream'''
-    # Clean up
     audio_stream.close()
     pa.terminate()
     porcupine.delete()
